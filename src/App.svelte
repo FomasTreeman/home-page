15ed66225b1aa3a00420c179608b0594db166f4b
FomasTreeman <tom@team-freeman.com>
2023-07-06 00:06:41 +0100

    search with either google or phin

--- src/App.svelte
+++ src/App.svelte
@@ -3,24 +3,39 @@
   import log from './lib/log';
   import Crypto from './lib/Crypto.svelte';
   import Github from './lib/Github.svelte';
+  import { onMount } from 'svelte';
+  let searchForm;
 
   function enterHandler(e) {
     if (e.key !== 'Enter') return;
-    const phindURL = `https://www.phind.com/search?q=${encodeURIComponent(
-      e.target.value
-    )}&source=searchbox`;
-    log(phindURL);
-    redirect(phindURL);
+    const input = encodeURIComponent(e.target.value);
+    let url;
+    switch (input[0]) {
+      case '/': {
+        url = `https://www.phind.com/search?q=${input}&source=searchbox`;
+      }
+      default: {
+        url = `https://www.google.com/search?q=${input}&sourceid=chrome&ie=UTF-8`;
+      }
+    }
+    log(url);
+    redirect(url);
   }
+
+  onMount(() => {
+    searchForm.focus();
+    // listen for hotkeys and do fun things
+  });
 </script>
 
 <main>
   <Crypto />
   <Github />
   <input
+    bind:this={searchForm}
     type="search"
     on:keyup={enterHandler}
-    placeholder="Search phind.com for  ..."
+    placeholder="Search google or phind.com for  ..."
   />
 </main>
 
